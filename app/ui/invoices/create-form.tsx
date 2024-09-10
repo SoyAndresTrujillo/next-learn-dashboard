25a4f71a026695ec10191b532bce1b1a00572111
soyandrestrujillo <andymania.1301@gmail.com>
2024-09-10 17:45:09 -0500

    feat(errors): Handling errors

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -6,12 +6,13 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-import type { CustomerField } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
+import type { CustomerField } from '@/app/lib/definitions';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
